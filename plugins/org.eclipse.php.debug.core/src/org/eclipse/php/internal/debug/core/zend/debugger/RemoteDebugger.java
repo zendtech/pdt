12c36bbf2167f5f59952d0b2646b2f7dc54e94f7
Natalia Bartol <natalia.b@zend.com>
2014-06-17 17:14:09 +0100

    Avoiding NPE.
    Signed-off-by: Natalia Bartol <natalia.b@zend.com>

--- plugins/org.eclipse.php.debug.core/src/org/eclipse/php/internal/debug/core/zend/debugger/RemoteDebugger.java
+++ plugins/org.eclipse.php.debug.core/src/org/eclipse/php/internal/debug/core/zend/debugger/RemoteDebugger.java
@@ -1267,9 +1267,11 @@ public class RemoteDebugger implements IRemoteDebugger {
 		if (project == null) {
 			String orginalURL = debugTarget.getLaunch().getAttribute(
 					IDebugParametersKeys.ORIGINAL_URL);
-			String projectName = new Path(orginalURL).segment(0);
-			IWorkspace workspace = ResourcesPlugin.getWorkspace();
-			project = workspace.getRoot().getProject(projectName);
+			if (orginalURL != null) {
+				String projectName = new Path(orginalURL).segment(0);
+				IWorkspace workspace = ResourcesPlugin.getWorkspace();
+				project = workspace.getRoot().getProject(projectName);
+			}
 		}
 		if (project != null) {
 			String projectName = project.getName();
