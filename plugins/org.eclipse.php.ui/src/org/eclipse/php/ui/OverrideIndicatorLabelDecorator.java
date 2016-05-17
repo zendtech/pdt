1809de7c5b7ce0ac6b651309f71b0bf03625c60f
Michal Niewrzal <michal.n@zend.com>
2016-05-17 12:17:06 +0200

    Bug 493762 - Many UI freeze due file parsing
    Conflicts:
    	plugins/org.eclipse.php.ui/src/org/eclipse/php/ui/OverrideIndicatorLabelDecorator.java
    
    Change-Id: Ib55ca811200f5c15022c99077337d8c3fd97a352

--- plugins/org.eclipse.php.ui/src/org/eclipse/php/ui/OverrideIndicatorLabelDecorator.java
+++ plugins/org.eclipse.php.ui/src/org/eclipse/php/ui/OverrideIndicatorLabelDecorator.java
@@ -12,14 +12,12 @@ import org.eclipse.jface.viewers.IDecoration;
 import org.eclipse.jface.viewers.ILabelDecorator;
 import org.eclipse.jface.viewers.ILabelProviderListener;
 import org.eclipse.jface.viewers.ILightweightLabelDecorator;
-import org.eclipse.php.internal.core.ast.nodes.*;
-import org.eclipse.php.internal.core.corext.dom.NodeFinder;
-import org.eclipse.php.internal.core.util.PHPBuildUtils;
+import org.eclipse.php.core.compiler.PHPFlags;
+import org.eclipse.php.internal.core.ast.nodes.IMethodBinding;
 import org.eclipse.php.internal.ui.PHPUiPlugin;
 import org.eclipse.php.internal.ui.corext.util.SuperTypeHierarchyCache;
 import org.eclipse.php.internal.ui.util.ImageDescriptorRegistry;
 import org.eclipse.php.internal.ui.util.ImageImageDescriptor;
-import org.eclipse.php.ui.editor.SharedASTProvider;
 import org.eclipse.swt.graphics.Image;
 import org.eclipse.swt.graphics.Point;
 import org.eclipse.swt.graphics.Rectangle;
@@ -134,11 +132,16 @@ public class OverrideIndicatorLabelDecorator implements ILabelDecorator, ILightw
 					return 0;
 				}
 
-				int flags = method.getFlags();
-				if (!method.isConstructor() && !Flags.isPrivate(flags) && !Flags.isStatic(flags)) {
-					int res = getOverrideIndicators(method);
-					return res;
+
+				if (!shouldCompute((IType) method.getParent())) {
+					return 0;
 				}
+
+				if (!shouldCompute(method)) {
+					return 0;
+				}
+
+				return getOverrideIndicators(method);
 			} catch (ModelException e) {
 				if (!e.isDoesNotExist()) {
 					PHPUiPlugin.log(e);
@@ -150,6 +153,16 @@ public class OverrideIndicatorLabelDecorator implements ILabelDecorator, ILightw
 		return 0;
 	}
 
+	private boolean shouldCompute(IType parent) throws ModelException {
+		return !PHPFlags.isInterface(parent.getFlags()) && parent.getSuperClasses() != null
+				&& parent.getSuperClasses().length != 0;
+	}
+
+	private boolean shouldCompute(IMethod method) throws ModelException {
+		int flags = method.getFlags();
+		return !method.isConstructor() && !Flags.isPrivate(flags) && !Flags.isStatic(flags);
+	}
+
 	/**
 	 * Note: This method is for internal use only. Clients should not call this
 	 * method.
@@ -157,28 +170,16 @@ public class OverrideIndicatorLabelDecorator implements ILabelDecorator, ILightw
 	 * @param method
 	 *            The element to decorate
 	 * @return Resulting decorations (combination of
-	 *         JavaElementImageDescriptor.IMPLEMENTS and
-	 *         JavaElementImageDescriptor.OVERRIDES)
+	 *         ScriptElementImageDescriptor.IMPLEMENTS and
+	 *         ScriptElementImageDescriptor.OVERRIDES)
 	 * @throws ModelException
 	 * @throws IOException
 	 * 
 	 * @noreference This method is not intended to be referenced by clients.
 	 */
 	protected int getOverrideIndicators(IMethod method) throws ModelException, IOException {
-		Program astRoot = SharedASTProvider.getAST(method.getSourceModule(), SharedASTProvider.WAIT_NO, null);
-		// XXX: WAIT_NO (instead of WAIT_ACTIVE_ONLY) due bug 443712 and until
-		// bug 438661 will be fixed (PHPReconcilingStrategy require access to
-		// UI thread)
-		if (astRoot != null) {
-			int res = findInHierarchyWithAST(astRoot, method);
-			if (res != -1) {
-				return res;
-			}
-		}
-
 		IType type = method.getDeclaringType();
-
-		if (type != null && !PHPBuildUtils.isIndexing()) {
+		if (type != null) {
 			MethodOverrideTester methodOverrideTester = SuperTypeHierarchyCache.getMethodOverrideTester(type);
 			IMethod defining = methodOverrideTester.findOverriddenMethod(method, true);
 			if (defining != null) {
@@ -192,24 +193,23 @@ public class OverrideIndicatorLabelDecorator implements ILabelDecorator, ILightw
 		return 0;
 	}
 
-	private int findInHierarchyWithAST(Program astRoot, IMethod method) throws ModelException {
-		SourceRange range = new SourceRange(method.getSourceRange().getOffset(), method.getSourceRange().getLength());
-		ASTNode node = NodeFinder.perform(astRoot, range);
-		if (node instanceof Identifier && node.getParent() instanceof MethodDeclaration) {
-			IMethodBinding binding = ((MethodDeclaration) node.getParent()).resolveMethodBinding();
-			if (binding != null) {
-				IMethodBinding defining = Bindings.findOverriddenMethod(binding, true);
-				if (defining != null) {
-					if (isAbstract(defining)) {
-						return ScriptElementImageDescriptor.IMPLEMENTS;
-					} else {
-						return ScriptElementImageDescriptor.OVERRIDES;
-					}
-				}
-				return 0;
-			}
+	/*
+	 * (non-Javadoc)
+	 * 
+	 * @see
+	 * org.eclipse.jface.viewers.ILightweightLabelDecorator#decorate(java.lang
+	 * .Object, org.eclipse.jface.viewers.IDecoration)
+	 */
+	@Override
+	public void decorate(Object element, IDecoration decoration) {
+		int adornmentFlags = computeAdornmentFlags(element);
+		if ((adornmentFlags & ScriptElementImageDescriptor.IMPLEMENTS) != 0) {
+
+			decoration.addOverlay(DLTKPluginImages.DESC_OVR_IMPLEMENTS);
+		} else if ((adornmentFlags & ScriptElementImageDescriptor.OVERRIDES) != 0) {
+
+			decoration.addOverlay(DLTKPluginImages.DESC_OVR_OVERRIDES);
 		}
-		return -1;
 	}
 
 	/*
